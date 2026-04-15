63ab52a9e6dcb41b58a42bfde2bb49257aee1599
ChristosTsepas <121814684+ChristosTsepas@users.noreply.github.com>
2026-04-15 12:49:52 +0200

    Initialize Basic_code.py

--- Basic_code.py
+++ Basic_code.py
@@ -1,1 +1,55 @@
+####################################
+#                                  #
+#    TISE PINN INVERSE PROBLEM     #
+#                                  #
+####################################
+
+# import necessary libs here
+
+
+# define model netwrork architectures
+
+# MLP data part ( I/O: wavefunctions -> energy states)
+# define # of layers
+# in_dims = 3 (r = x,y,z) (T.B.D)
+# hidd_dims = N
+# out_dims = 1 (T.B.D SEE THE INPUT)
+# activation function = SIREN (depending on the input)
+
+
+# MLP PHYSICS (WS parameters) (I/O: energy (T.B.D) -> N parameters of WS
+# define N layers
+# in_dims = 1 (E)
+# hidd_dims = N
+# out_dims = N
+# activation function (
+
+
+
+
+# define optimizer
+# define loss (for now MSE)
+
+
+
+############## Training loop #################
+
+# optimizer_zero grad
+
+# models
+
+# calculate L = αLdata + βLphys
+
+# Loss backward
+
+# optimizer step
+
+
+
+################ Evaluation #################33
+
+# plot parameter maps
+# print energy states
+# create GT parameter maps and calculate NMSE
+
 
